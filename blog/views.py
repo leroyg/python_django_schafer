bacc79276b27d677b3e09668f1912739e470930e
leroyg <leroy.gardner@gmail.com>
2020-06-28 01:47:29 +0300

    updated admin, post views, and datetime

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'LeRoyG',
-        'title': 'Blog Post',
-        'content': 'First Post Content',
-        'date_posted': 'June 26, 2020'
-    },
-    {
-        'author': 'JackJ',
-        'title': 'Blog Post Too',
-        'content': '2nd Post Content',
-        'date_posted': 'June 29, 2020'
-    },
-    {
-        'author': 'SheenaE',
-        'title': 'Blog Post Three',
-        'content': '3rd Post Content',
-        'date_posted': 'June 22, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
